f688d338c9e14a51c89a13820a89bf25946811fc
mydatakusy-glitch <mydatakusy@gmail.com>
2026-05-24 22:12:25 +0800

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 // Service to interact with Google Apps Script Backend
 
 // To use the real backend, replace this URL with your deployed Google Apps Script Web App URL
-const GAS_WEB_APP_URL = ""; 
+const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbyN6ZCHMtn6nqGKWBoMVChmuc1wRfeoVOQ5Ogz9Yp_TqjGBdJr5GJ425ScozXq7N2qF/exec"; 
 
 export interface FormField {
   id: string;
